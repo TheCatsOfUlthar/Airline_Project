54b1153c1058203f920144074fca573c9bb62cb4
SamKT17 <skthomas0339@eagle.fgcu.edu>
2021-04-05 11:42:05 -0400

    Commented the test cases.

--- src/test/java/UpdateCustomerTest.java
+++ src/test/java/UpdateCustomerTest.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -7,24 +6,54 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-
+/** The Update Customer Test, testing requirement:
+ *
+ * "The reservation system shall allow a customer account to be edited and updated."
+ */
 public class UpdateCustomerTest {
 
+    /** Search Customer Object. */
     private searchCustomer searchCustomer;
 
+    /**
+     * The before each declarator allows a certain action to happen before each test case. In this
+     * case, we are creating a new searchCustomer class instantiation.
+     */
     @BeforeEach
     void setUp() {
         searchCustomer = new searchCustomer();
     }
 
+    /**
+     * Negative Test
+     *
+     * This test case will pass if the customer that is being updated is a valid customer in the
+     * database.
+     *
+     * @param ID - The ID of the user being updated
+     * @param firstName - the first name of the user being updated
+     * @param lastName - the last name of the user being updated
+     * @param NIC - this is the NIC number being updated
+     * @param passport - this is the passport number being updated
+     * @param address - the address of the person being updated
+     * @param date - this is the of the user being updated
+     * @param gender - the gender of the person being updated
+     * @param contact - this is the phone number of the person being updated
+     */
     @ParameterizedTest
     @MethodSource("testMethodSource")
-    public void shouldThrowException(String ID, String firstName, String lastName, String NIC,
+    public void updateUserTest(String ID, String firstName, String lastName, String NIC,
                                      String passport, String address, String date, String gender,
                                      String contact) {
        searchCustomer.updateCustomer(ID, firstName, lastName, NIC, passport, address, date, gender, contact);
     }
 
+    /**
+     * This method creates a stream of arguments, so multiple parameter methods can be tested. This is
+     * where the inputs for the test case comes from.
+     *
+     * @return - a stream of arguments
+     */
     static Stream<Arguments> testMethodSource() {
         return Stream.of(
                 Arguments.arguments("", "Sam", "Thomas", "12345678", "123456789", "12720", "1997-17-08", "Male", "12341234"),
@@ -32,6 +61,11 @@ public class UpdateCustomerTest {
         );
     }
 
+    /**
+     * The after each declarator allows an action to be performed after each test case. Here we are
+     * setting the searchCustomer instantiation for each test case to null which will save memory by
+     * allowing the garbage collector to clean up faster.
+     */
     @AfterEach
     void tearDown() {
         searchCustomer = null;
--- src/test/java/searchCustomerTest.java
+++ src/test/java/searchCustomerTest.java
@@ -1,17 +1,25 @@
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import javax.swing.*;
 
+/** Search Customer Test, testing requirement:
+ *
+ *  "The reservation system shall allow users to search for a customer."
+ */
 public class searchCustomerTest {
 
+    // Setting up the objects to be used
     private searchCustomer searchCustomer;
     private JLabel txtPhoto;
 
+    /**
+     * The before each declarator allows a certain action to happen before each test case. In this
+     * case, we are creating a new searchCustomer class instantiation and creating a JLabel to pass
+     * into the method being tested.
+     */
     @BeforeEach
     void setUp() {
         searchCustomer = new searchCustomer();
@@ -19,18 +27,24 @@ public class searchCustomerTest {
         txtPhoto.setSize(100, 100);
     }
 
+    /**
+     * Negative Test
+     *
+     * This test case tests to see if the user exists in the database.
+     *
+     * @param id - this is ID of the customer that is being searched
+     */
     @ParameterizedTest
     @ValueSource(strings = {"CS001", "CS002", "CS003", ""})
     void doesCustomerExist(String id) {
         searchCustomer.searchCustomerInformation(id, txtPhoto);
     }
 
-/*    @Test
-    void shouldThrowException() {
-        Assertions.assertThrows(ClassNotFoundException.class, () ->
-                searchCustomer.searchCustomerInformation("", txtPhoto));
-    }*/
-
+    /**
+     * The after each declarator allows an action to be performed after each test case. Here we are
+     * setting the searchCustomer instantiation for each test case to null which will save memory by allowing
+     * the garbage collector to clean up faster.
+     */
     @AfterEach
     void tearDown() {
         searchCustomer = null;
